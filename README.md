b700b459e9963a92f2765f530ee37a996ef0edb3
Luke Merrett <lukeamerrett@gmail.com>
2013-07-26 11:38:01 +0100

    Unit tested EncryptionService
    Added suite of unit tests covering the EncryptionService

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Bouncy-Castle-AES-GCM-Encryption
 
 Initial code based off this [Stack Overflow question](http://codereview.stackexchange.com/questions/14892/review-of-simplified-secure-encryption-of-a-string) by James Tuley.
 
-
+This code sample shows how to use the [BouncyCastle](https://www.nuget.org/packages/BouncyCastle/) library to perform encryption and decryption using AES-GCM 256bit.
 
 
--- Unit Tests/EncryptionServiceTests.cs
+++ Unit Tests/EncryptionServiceTests.cs
@@ -28,13 +28,131 @@
         }
 
         [Test]
-        public void NewKey_MethodRun_ReturnsRandomKey()
+        public void EncryptWithKey_EmptyMessageProvided_ArgumentExceptionThrown()
         {
-            var twoFiveSixBitKey = _encryptionService.NewKey();
+            string message = string.Empty;
 
-            var stringKey = Convert.ToBase64String(twoFiveSixBitKey);
+            var key = _encryptionService.NewKey();
 
-            Assert.IsFalse(string.IsNullOrWhiteSpace(stringKey));
+            Assert.Throws<ArgumentException>(() => _encryptionService.EncryptWithKey(message, key));
+        }
+
+        [Test]
+        public void EncryptWithKey_KeyProvidedIsNotA256BitKey_ArgumentExceptionThrown()
+        {
+            const string Message = "EncryptMe";
+
+            var random = new Random();
+            var key = new Byte[16];
+
+            random.NextBytes(key);
+
+            var encodedKey = Convert.ToBase64String(key);
+
+            Assert.Throws<ArgumentException>(() => _encryptionService.EncryptWithKey(Message, encodedKey));
+        }
+
+        [Test]
+        public void EncryptWithKey_NullKeyProvided_ArgumentNullExceptionThrown()
+        {
+            const string Message = "EncryptMe";
+
+            Assert.Throws<ArgumentNullException>(() => _encryptionService.EncryptWithKey(Message, null));
+        }
+
+        [Test]
+        public void EncryptWithKey_NullMessageProvided_ArgumentExceptionThrown()
+        {
+            var key = _encryptionService.NewKey();
+
+            Assert.Throws<ArgumentException>(() => _encryptionService.EncryptWithKey(null, key));
+        }
+
+        [Test]
+        public void EncryptWithKey_ValidKeyAndMessageProvided_MessageIsEncrypted()
+        {
+            const string Message = "EncryptMe";
+
+            var key = _encryptionService.NewKey();
+
+            var encryptedText = _encryptionService.EncryptWithKey(Message, key);
+
+            Assert.IsFalse(string.IsNullOrEmpty(encryptedText));
+        }
+
+        [Test]
+        public void DecryptWithKey_EmptyMessageProvided_ArgumentExceptionThrown()
+        {
+            var message = string.Empty;
+
+            var key = _encryptionService.NewKey();
+
+            Assert.Throws<ArgumentException>(() => _encryptionService.DecryptWithKey(message, key));
+        }
+
+        [Test]
+        public void DecryptWithKey_NullKeyProvided_ArgumentNullExceptionThrown()
+        {
+            const string Message = "DecryptMe";
+
+            Assert.Throws<ArgumentNullException>(() => _encryptionService.DecryptWithKey(Message, null));
+        }
+
+        [Test]
+        public void DecryptWithKey_NullMessageProvided_ArgumentExceptionThrown()
+        {
+            var key = _encryptionService.NewKey();
+
+            Assert.Throws<ArgumentException>(() => _encryptionService.DecryptWithKey(null, key));
+        }
+
+        [Test]
+        public void DecryptWithKey_KeyProvidedIsNotA256BitKey_FormatExceptionThrown()
+        {
+            const string Message = "DecryptMe";
+
+            var random = new Random();
+            var key = new Byte[16];
+
+            random.NextBytes(key);
+
+            var encodedKey = Convert.ToBase64String(key);
+
+            Assert.Throws<FormatException>(() => _encryptionService.DecryptWithKey(Message, encodedKey));
+        }
+
+        [Test]
+        public void DecryptWithKey_ValidKeyAndMessageProvided_ArgumentExceptionThrown()
+        {
+            const string Message = "EncryptMe";
+
+            var key = _encryptionService.NewKey();
+
+            var encryptedString = _encryptionService.EncryptWithKey(Message, key);
+
+            var decryptedString = _encryptionService.DecryptWithKey(encryptedString, key);
+
+            Assert.AreEqual(Message, decryptedString);
+        }
+
+        [Test]
+        public void NewKey_KeyReturned_KeyCanBeCastToABase64String()
+        {
+            var key = _encryptionService.NewKey();
+
+            Assert.IsFalse(string.IsNullOrWhiteSpace(key));
+        }
+
+        [Test]
+        public void NewKey_KeyReturned_KeyIs256BitKey()
+        {
+            var key = _encryptionService.NewKey();
+
+            var decodedKey = Convert.FromBase64String(key);
+
+            var numberOfBits = decodedKey.Length * 8;
+
+            Assert.AreEqual(256, numberOfBits);
         }
 
         #endregion
